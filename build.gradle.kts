e4e8adad0b214ee3fa2665fe0ecac7161744e9aa
cleefsouza <a.cleef.souza@gmail.com>
2021-12-21 14:39:58 -0300

    Base da demo-api

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val exposedVersion: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.10"
+                id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+}
+
+group = "com.religans"
+version = "0.0.1"
+application {
+    mainClass.set("com.religans.ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-serialization:$ktor_version")
+    implementation("io.ktor:ktor-gson:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+}
---
+++ src/test/kotlin/com/religans/ApplicationTest.kt
@@ -0,0 +1,25 @@
+package com.religans
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.serialization.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import com.religans.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ configureRouting() }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("Hello World!", response.content)
+            }
+        }
+    }
+}
